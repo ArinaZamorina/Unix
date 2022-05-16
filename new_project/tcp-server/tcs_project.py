eb029ff0e3f57990f79845d5210324ec9c0450df
Arina <arinazam98@mail.ru>
2022-05-16 11:27:54 +0300

    logging

--- new_project/tcp-server/tcs_project.py
+++ new_project/tcp-server/tcs_project.py
@@ -1,13 +1,20 @@
 #создать простой TCP сервер, который принимает от клиента строку и возвращает ее
 
+import socket
+import logging
+
 sock = socket.socket()
 sock.bind((input('Server host: ', ''), input('Server port: ') or 9090))
+logging.info('Прослушивание порта')
 sock.listen(1)
+logging.info('Подключение')
 conn, addr = sock.accept()
+logging.info('Приём данных')
 while True:
     data = conn.recv(1024)
     if not data:
         break
+    logging.info('Отправка данных')
     conn.send(data.upper())
 
 conn.close()
@@ -17,15 +24,18 @@ conn.close()
 #2
 import socket
 sock = socket.socket()
+logging.info('Подключение')
 sock.connect((input('Host: ' or 'localhost'), input('Port: ' or '9090')))
 while True:
     i = input()
     if i != 'exit':
+        logging.info('Отправление данных')
         sock.send(i)
     else:
         break
 
 data = sock.recv(1024)
+logging.info('Остановка сервера')
 sock.close()
 
 print(data)
