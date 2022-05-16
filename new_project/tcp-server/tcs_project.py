cc339cb73de16f2c5cdf7843b419f55b27ae9d5e
Arina <arinazam98@mail.ru>
2022-05-16 10:46:31 +0300

    add a new project file(tpc)

---
+++ new_project/tcp-server/tcs_project.py
@@ -0,0 +1,35 @@
+#создать простой TCP сервер, который принимает от клиента строку и возвращает ее
+
+sock = socket.socket()
+sock.bind(('', 9090))
+sock.listen(1)
+conn, addr = sock.accept()
+while True:
+    data = conn.recv(1024)
+    if not data:
+        break
+    conn.send(data.upper())
+
+conn.close()
+
+
+# создать сокет, подключиться к серверу, послать данные, принять и закрыть
+#2
+import socket
+sock = socket.socket()
+sock.connect(('localhost', 9090))
+while True:
+    i = input()
+    if i != 'exit':
+        sock.send(i)
+    else:
+        break
+
+data = sock.recv(1024)
+sock.close()
+
+print(data)
+
+
+
+
